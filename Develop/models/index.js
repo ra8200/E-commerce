3870bf2b1d5a741ab12776c21f8440f3a46aec37
ra8200 <ra8200@hotmail.com>
2021-07-24 11:41:30 -0400

    5th push

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: cascade,
+});
 // Categories have many Products
-
+Category.belongsTo(Product, {
+  foreignKey: "category_id",
+  onDelete: cascade,
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  foreignKey: "product_id",
+  onDelete: cascade,
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+  onDelete: cascade,
+});
 
 module.exports = {
   Product,
