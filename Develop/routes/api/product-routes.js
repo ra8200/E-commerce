dc5fc5feb8a5dc70231d8db3cacc9ec241e25961
ra8200 <ra8200@hotmail.com>
2021-07-24 11:55:05 -0400

    7th push

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll ({
+    include: [Category,{
+      model:Tag,
+    through:ProductTag}]
+  }) .then ( records => {
+       console.log("record",records);
+       res.json(records)
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findAll ({
+    where:{
+      id:req.params.is},
+    include: [Category,{
+      model:Tag,
+    through:ProductTag}]
+  }) .then ( records => {
+       console.log("record",records);
+       res.json(records)
+  });
 });
 
 // create new product
@@ -91,6 +109,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy ({
+    where:{
+      id:req.params.id}  
+  }) .then ( records => {
+       console.log("record",records);
+       res.json(records)
+  });
 });
 
 module.exports = router;
